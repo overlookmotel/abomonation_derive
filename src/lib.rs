7c3178b97df5c6e0278adbfeda0d915583defcf3
overlookmotel <theoverlookmotel@gmail.com>
2023-02-24 01:54:53 +0000

    `unsafe_abomonate_proxy` attr

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![recursion_limit="128"]
 
+use proc_macro2::TokenStream;
+use syn::{Meta, MetaList, NestedMeta, Path};
 use synstructure::decl_derive;
 use quote::quote;
 
-decl_derive!([Abomonation, attributes(unsafe_abomonate_ignore)] => derive_abomonation);
+decl_derive!([Abomonation, attributes(unsafe_abomonate_ignore, unsafe_abomonate_proxy)] => derive_abomonation);
 
 fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStream {
     s.filter(|bi| {
@@ -13,19 +15,28 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
             .any(|attr| attr.path().is_ident("unsafe_abomonate_ignore"))
     });
 
-    let entomb = s.each(|bi| quote! {
-        ::abomonation::Abomonation::entomb(#bi, _write)?;
+    let entomb = s.each(|bi| {
+        let bi = wrap_with_proxy(bi);
+        quote! {
+            ::abomonation::Abomonation::entomb(#bi, _write)?;
+        }
     });
 
-    let extent = s.each(|bi| quote! {
-        sum += ::abomonation::Abomonation::extent(#bi);
+    let extent = s.each(|bi| {
+        let bi = wrap_with_proxy(bi);
+        quote! {
+            sum += ::abomonation::Abomonation::extent(#bi);
+        }
     });
 
     s.bind_with(|_| synstructure::BindStyle::RefMut);
 
-    let exhume = s.each(|bi| quote! {
-        let temp = bytes;
-        bytes = ::abomonation::Abomonation::exhume(#bi, temp)?;
+    let exhume = s.each(|bi| {
+        let bi = unwrap_with_proxy(bi);
+        quote! {
+            let temp = bytes;
+            bytes = ::abomonation::Abomonation::exhume(#bi, temp)?;
+        }
     });
 
     s.bound_impl(quote!(abomonation::Abomonation), quote! {
@@ -48,4 +59,43 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
             Some(bytes)
         }
     })
+}
+
+fn wrap_with_proxy(bi: &synstructure::BindingInfo) -> TokenStream {
+    let proxy = get_proxy(bi);
+    if let Some(path) = proxy {
+        quote! { &#path::wrap(#bi) }
+    } else {
+        quote! { #bi }
+    }
+}
+
+#[allow(dead_code)]
+fn unwrap_with_proxy(bi: &synstructure::BindingInfo) -> TokenStream {
+    let proxy = get_proxy(bi);
+    if let Some(path) = proxy {
+        quote! { &mut #path::unwrap(#bi) }
+    } else {
+        quote! { #bi }
+    }
+}
+
+fn get_proxy(bi: &synstructure::BindingInfo) -> Option<Path> {
+    bi.ast().attrs.iter()
+        .map(|attr| attr.parse_meta())
+        .filter_map(Result::ok)
+        .filter(|attr| attr.path().is_ident("unsafe_abomonate_proxy"))
+        .map(|attr| {
+            if let Meta::List(MetaList {nested, ..}) = attr {
+                let parts: Vec<NestedMeta> = nested.into_iter().collect();
+                if parts.len() == 1 {
+                    let first = parts.into_iter().nth(0).unwrap();
+                    if let NestedMeta::Meta(Meta::Path(path)) = first {
+                        return path;
+                    }
+                }
+            }
+            panic!("unsafe_abomonate_proxy needs a path");
+        })
+        .nth(0)
 }
