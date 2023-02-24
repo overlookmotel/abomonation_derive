37c78dcadf93114de463bd3524bafb82fbb952e7
overlookmotel <theoverlookmotel@gmail.com>
2023-02-24 21:09:41 +0000

    `unsafe_abomonate_with` attr

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 #![recursion_limit="128"]
 
-use proc_macro2::TokenStream;
 use syn::{Meta, MetaList, NestedMeta, Path};
 use synstructure::decl_derive;
 use quote::quote;
 
-decl_derive!([Abomonation, attributes(unsafe_abomonate_ignore, unsafe_abomonate_proxy)] => derive_abomonation);
+decl_derive!([Abomonation, attributes(unsafe_abomonate_ignore, unsafe_abomonate_with)] => derive_abomonation);
 
 fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStream {
     s.filter(|bi| {
@@ -16,26 +15,39 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
     });
 
     let entomb = s.each(|bi| {
-        let bi = wrap_with_proxy(bi);
-        quote! {
-            ::abomonation::Abomonation::entomb(#bi, _write)?;
+        match get_with(bi) {
+            Some(proxy) => quote! {
+                #proxy::entomb_with(#bi, _write)?;
+            },
+            None => quote! {
+                ::abomonation::Abomonation::entomb(#bi, _write)?;
+            }
         }
     });
 
     let extent = s.each(|bi| {
-        let bi = wrap_with_proxy(bi);
-        quote! {
-            sum += ::abomonation::Abomonation::extent(#bi);
+        match get_with(bi) {
+            Some(proxy) => quote! {
+                sum += #proxy::extent_with(#bi);
+            },
+            None => quote! {
+                sum += ::abomonation::Abomonation::extent(#bi);
+            }
         }
     });
 
     s.bind_with(|_| synstructure::BindStyle::RefMut);
 
     let exhume = s.each(|bi| {
-        let bi = unwrap_with_proxy(bi);
-        quote! {
-            let temp = bytes;
-            bytes = ::abomonation::Abomonation::exhume(#bi, temp)?;
+        match get_with(bi) {
+            Some(proxy) => quote! {
+                let temp = bytes;
+                bytes = #proxy::exhume_with(#bi, temp)?;
+            },
+            None => quote! {
+                let temp = bytes;
+                bytes = ::abomonation::Abomonation::exhume(#bi, temp)?;
+            }
         }
     });
 
@@ -61,30 +73,11 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
     })
 }
 
-fn wrap_with_proxy(bi: &synstructure::BindingInfo) -> TokenStream {
-    let proxy = get_proxy(bi);
-    if let Some(path) = proxy {
-        quote! { &#path::wrap(#bi) }
-    } else {
-        quote! { #bi }
-    }
-}
-
-#[allow(dead_code)]
-fn unwrap_with_proxy(bi: &synstructure::BindingInfo) -> TokenStream {
-    let proxy = get_proxy(bi);
-    if let Some(path) = proxy {
-        quote! { &mut #path::unwrap(#bi) }
-    } else {
-        quote! { #bi }
-    }
-}
-
-fn get_proxy(bi: &synstructure::BindingInfo) -> Option<Path> {
+fn get_with(bi: &synstructure::BindingInfo) -> Option<Path> {
     bi.ast().attrs.iter()
         .map(|attr| attr.parse_meta())
         .filter_map(Result::ok)
-        .filter(|attr| attr.path().is_ident("unsafe_abomonate_proxy"))
+        .filter(|attr| attr.path().is_ident("unsafe_abomonate_with"))
         .map(|attr| {
             if let Meta::List(MetaList {nested, ..}) = attr {
                 let parts: Vec<NestedMeta> = nested.into_iter().collect();
@@ -95,7 +88,7 @@ fn get_proxy(bi: &synstructure::BindingInfo) -> Option<Path> {
                     }
                 }
             }
-            panic!("unsafe_abomonate_proxy needs a path");
+            panic!("unsafe_abomonate_with needs a path");
         })
         .nth(0)
 }
